63bfd2a5cef598a86d3a0c6511579b6eb832675f
kono94 <jan.loewenstrom.com>
2016-06-14 18:03:55 +0200

    deleting stuff

--- includes/inc_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// error_reporting(E_ALL);
-// ini_set("display_errors", 1);
-//ini_set('include_path', realpath($_SERVER["DOCUMENT_ROOT"]).'/lcsmanager_beta/public_html/');
-define("DB_USERNAME", "");
-define("DB_NAME", "");
-define("DB_HOST", "");
-define("DB_PASSWORD", "");
-?>
